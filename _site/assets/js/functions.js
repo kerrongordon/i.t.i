4007d9a7b426a940c951658d722ad887f26a112c
kerrongordon <kgpsounds.com@gmail.com>
2014-09-25 21:28:19 -0400

    added client-testimonials

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -30,4 +30,51 @@ $(document).ready(function() {
       };
     rippleEffect = new $.RippleEffect(options);
   });
+
+
+
+
+
+  //create the slider
+  $('.cd-testimonials-wrapper').flexslider({
+    selector: ".cd-testimonials > li",
+    animation: "slide",
+    controlNav: false,
+    slideshow: true,
+    smoothHeight: true,
+    start: function(){
+      $('.cd-testimonials').children('li').css({
+        'opacity': 1,
+        'position': 'relative'
+      });
+    }
+  });
+
+  
+
+  //open the testimonials modal page
+  $('.cd-see-all').click(function(){
+    $('.cd-testimonials-all').addClass('is-visible');
+    $('.cd-testimonials-wrapper').css("height", cdTestimonialsAll + 'px');
+  });
+
+  //close the testimonials modal page
+  $('.cd-testimonials-all .close-btn').on('click', function(){
+    $('.cd-testimonials-all').removeClass('is-visible');
+    $('.cd-testimonials-wrapper').removeAttr('style');
+  });
+  $(document).keyup(function(event){
+    //check if user has pressed 'Esc'
+      if(event.which=='27'){
+        $('.cd-testimonials-all').removeClass('is-visible');  
+      }
+    });
+    
+  //build the grid for the testimonials modal page
+  $('.cd-testimonials-all-wrapper').children('ul').masonry({
+      itemSelector: '.cd-testimonials-item'
+  });
+
+  var cdTestimonialsAll = $('.cd-testimonials-all-wrapper ul').height();
+  
 });
